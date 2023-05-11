b05131c021b34d5402ccf88e0933e663ac2c02a5
Taylor Murdock <tjmurd4545@gmail.com>
2023-05-11 14:35:52 -0400

    "user can use scissors to cut grass"

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 // LANDSCAPER
-let tool= "teeth"
+let tool = "teeth"
 let wallet = 0;
 
 const cutGrass = () => {
     if (tool === "teeth") {
         wallet += 1;
         console.log(`You just made $1 for cutting that yard with your ${tool}! The total amount in your wallet is now $${wallet}.`)
+    } else {
+        wallet += 5
+        console.log(`You just made $5 for cutting that yard with your ${tool}! The total amount in your wallet is now $${wallet}.`)
     }
     
 }
 
 const buyRustyScissors = () => {
-    if (wallet += 5 && tool === "teeth") {
-        tool === "rusty scissors";
-        wallet -= 6;
+    if (wallet >= 5 && tool === "teeth") {
+        tool = "rusty scissors";
+        wallet -= 5;
         console.log(`You bought some scissors for $5! You now have a ${tool} to use and you also now have $${wallet}`)
-    } else if (tool === "rusty scissprs") {
+    } else if (tool === "rusty scissors") {
         console.log(`you already have a ${tool} as a tool`)
     } else {
         console.log(`You only have $${wallet}. Cute more yards to save more money.`)
@@ -27,3 +30,5 @@ cutGrass()
 cutGrass()
 cutGrass()
 buyRustyScissors()
+cutGrass()
+cutGrass()
