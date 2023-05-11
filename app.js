f12844f5e5a2fcca381e96ede34830ec0b2fd92f
Taylor Murdock <tjmurd4545@gmail.com>
2023-05-11 14:24:59 -0400

    "landscaper: user can buy scissors"

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 // LANDSCAPER
-
+let tool= "teeth"
 let wallet = 0;
 
 const cutGrass = () => {
-    wallet += 1;
-    console.log(`You just made $1 for cutting that yard! The total amount in your wallet is now $${wallet}.`)
+    if (tool === "teeth") {
+        wallet += 1;
+        console.log(`You just made $1 for cutting that yard with your ${tool}! The total amount in your wallet is now $${wallet}.`)
+    }
+    
 }
 
-cutGrass();
-cutGrass();
-cutGrass();
+const buyRustyScissors = () => {
+    if (wallet += 5 && tool === "teeth") {
+        tool === "rusty scissors";
+        wallet -= 6;
+        console.log(`You bought some scissors for $5! You now have a ${tool} to use and you also now have $${wallet}`)
+    } else if (tool === "rusty scissprs") {
+        console.log(`you already have a ${tool} as a tool`)
+    } else {
+        console.log(`You only have $${wallet}. Cute more yards to save more money.`)
+    }
+}
+cutGrass()
+cutGrass()
+cutGrass()
+cutGrass()
+cutGrass()
+buyRustyScissors()
