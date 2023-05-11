3b229c67f561f05523f985bb18499b081553e24e
Taylor Murdock <tjmurd4545@gmail.com>
2023-05-11 14:08:55 -0400

    "landscaper: user can use teeth to cut grass"

---
+++ app.js
@@ -0,0 +1,12 @@
+// LANDSCAPER
+
+let wallet = 0;
+
+const cutGrass = () => {
+    wallet += 1;
+    console.log(`You just made $1 for cutting that yard! The total amount in your wallet is now $${wallet}.`)
+}
+
+cutGrass();
+cutGrass();
+cutGrass();
