e51ec8620a2dd56cb31a48fb7b1520eee6c500fc
Taylor Murdock <tjmurd4545@gmail.com>
2023-05-12 12:06:17 -0400

    "projecting now"

--- app.js
+++ app.js
@@ -1,102 +1,75 @@
-// LANDSCAPER
-let tool = "teeth";
+let tools = ["teeth"];
 let wallet = 0;
 
+const resetGame = () => {
+  tools = ["teeth"];
+  wallet = 0;
+  console.log(
+    `You have decided to reset the game. You now have ${tools} as a tool and ${wallet} in your wallet`
+  );
+};
+
 const cutGrass = () => {
-  if (tool === "teeth") {
-    wallet += 1;
-    console.log(
-      `You just made $1 for cutting that yard with your ${tool}! The total amount in your wallet is now $${wallet}.`
-    );
-  } else if (tool === "rusty scissors") {
-    wallet += 5;
-    console.log(
-      `You just made $5 for cutting that yard with your ${tool}! The total amount in your wallet is now $${wallet}.`
-    );
-  } else if (tool === "old push mower") {
-    wallet += 50;
-    console.log(
-      `You just made $50 for cutting that yard with your ${tool}! The total amount in your wallet is now $${wallet}.`
-    );
-  } else if (tool === "battery powered mower") {
-    wallet += 100;
-    console.log(
-      `You just made $100 for cutting that yard with your ${tool}! The total amount in your wallet is now $${wallet}.`
-    );
-  } else if (tool === "starving students") {
-    wallet += 250;
-    console.log(
-      `You just made $250 for having the ${tool} cut that yard for you! The total amount in your wallet is now $${wallet}.`
-    );
+  let earnings = 1;
+  if (tools.includes("rusty scissors")) {
+    earnings += 5;
   }
-  if (wallet >= 1000) {
+  if (tools.includes("old push mower")) {
+    earnings += 50;
+  }
+  if (tools.includes("battery powered mower")) {
+    earnings += 100;
+  }
+  if (tools.includes("starving students")) {
+    earnings += 250;
+  }
+  wallet += earnings;
+  console.log(
+    `You just made $${earnings} for cutting that yard with your ${tools.join(
+      ", "
+    )}! The total amount in your wallet is now $${wallet}.`
+  );
+  if (wallet >= 10000) {
     console.log("You have won the game LandScaper");
     return;
   }
 };
 
-const buyRustyScissors = () => {
-  if (wallet >= 5 && tool === "teeth") {
-    tool = "rusty scissors";
-    wallet -= 5;
+const buyTool = (toolName, cost) => {
+  if (wallet >= cost) {
+    tools.push(toolName);
+    wallet -= cost;
     console.log(
-      `You bought some scissors for $5! You now have a ${tool} to use and you also now have $${wallet}`
+      `You bought a ${toolName} for $${cost}! You now have ${tools.join(
+        ", "
+      )} as tools and you also now have $${wallet}`
     );
-  } else if (tool === "rusty scissors") {
-    console.log(`you already have a ${tool} as a tool`);
   } else {
-    console.log(
-      `You only have $${wallet}. Cute more yards to save more money.`
-    );
+    console.log(`You only have $${wallet}. Cut more yards to save more money.`);
   }
 };
 
-const buyOldPushMower = () => {
-  if (wallet >= 25 && tool === "rusty scissors") {
-    tool = "old push mower";
-    wallet -= 25;
+const sellTool = (toolName) => {
+  if (tools.includes(toolName)) {
+    const toolIndex = tools.indexOf(toolName);
+    const toolPrice = Math.floor(toolPrices[toolName] / 2);
+    wallet += toolPrice;
+    tools.splice(toolIndex, 1);
     console.log(
-      `You bought an old push mower for $25! You now have a ${tool} to use and you also now have $${wallet}`
+      `You sold your ${toolName} for $${toolPrice}! You now have ${tools.join(
+        ", "
+      )} as tools and you also now have $${wallet}`
     );
-  } else if (tool === "old push mower") {
-    console.log(`you already have a ${tool} as a tool`);
   } else {
-    console.log(
-      `You only have $${wallet}. Cute more yards to save more money.`
-    );
+    console.log(`You do not own a ${toolName} to sell.`);
   }
 };
 
-const buyBattPoweredMower = () => {
-  if (wallet >= 250 && tool === "old push mower") {
-    tool = "battery powered mower";
-    wallet -= 250;
-    console.log(
-      `You bought a battery powered mower for $250! You now have a ${tool} to use and you also now have $${wallet}`
-    );
-  } else if (tool === "battery powered mower") {
-    console.log(`you already have a ${tool} as a tool`);
-  } else {
-    console.log(
-      `You only have $${wallet}. Cute more yards to save more money.`
-    );
-  }
-};
-
-const hireStarvingStudent = () => {
-  if (wallet >= 500 && tool === "battery powered mower") {
-    tool = "starving students";
-    wallet -= 500;
-    console.log(
-      `You hired a team of starving students for $500! You now have a team of ${tool} to cut grass for you and you also now have $${wallet}`
-    );
-  } else if (tool === "starving students") {
-    console.log(`you already have a ${tool} as a tool`);
-  } else {
-    console.log(
-      `You only have $${wallet}. Cute more yards to save more money.`
-    );
-  }
+const toolPrices = {
+  "rusty scissors": 5,
+  "old push mower": 25,
+  "battery powered mower": 250,
+  "starving students": 500,
 };
 
 cutGrass();
@@ -104,26 +77,34 @@ cutGrass();
 cutGrass();
 cutGrass();
 cutGrass();
-buyRustyScissors();
+buyTool("rusty scissors", 5);
 cutGrass();
 cutGrass();
 cutGrass();
 cutGrass();
 cutGrass();
-buyOldPushMower();
+buyTool("old push mower", 25);
+cutGrass();
+cutGrass();
+buyTool("old push mower", 25);
+cutGrass();
+cutGrass();
+sellTool("old push mower", 25);
+cutGrass();
+buyTool("battery powered mower", 250);
 cutGrass();
 cutGrass();
 cutGrass();
 cutGrass();
 cutGrass();
-buyBattPoweredMower();
+buyTool("starving students", 500);
+cutGrass();
+cutGrass();
+sellTool("battery powered mower", 250);
+sellTool("old push mower", 25);
+sellTool("rusty scissors", 5);
 cutGrass();
 cutGrass();
 cutGrass();
-cutGrass();
-cutGrass();
-hireStarvingStudent();
-cutGrass();
-cutGrass();
-cutGrass();
+resetGame();
 cutGrass();
